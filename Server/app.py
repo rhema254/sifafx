ad1039811057f58ad8030c34c12f066be219e506
rhema254 <karhem254@gmail.com>
2024-09-30 02:16:04 +0300

    update server configs

--- Server/app.py
+++ Server/app.py
@@ -3,7 +3,7 @@ import google.oauth2.credentials
 import google_auth_oauthlib.flow
 import googleapiclient.discovery
 from datetime import datetime, timedelta
-from config import DevConfig
+from Server.config import DevConfig
 from decouple import config
 from flask_restx import Api, Resource, fields
 from flask_cors import CORS
